cb4741c220aad5b7dc9ceaf46bcf9a3625c205c7
JohnnyTh <userboxj@gmail.com>
2025-10-24 11:24:44 +0300

    add validation layer

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-build:
-	mkdir -p build_ && cd build_ && cmake .. && cmake --build .
+build_release:
+	mkdir -p build_ && cd build_ && cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+
+build_test:
+	mkdir -p build_ && cd build_ && cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build .
 
 .PHONY: clean
 clean:
 	rm build_/cv_tricks_vulkan -f
 
-test: build
+run_release: build_release
+	./build_/cv_tricks_vulkan
+
+run_test: build_test
 	./build_/cv_tricks_vulkan
--- main.cpp
+++ main.cpp
@@ -5,9 +5,63 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <cstring>
 
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
+
+const std::vector<const char *> validationLayers = {"VK_LAYER_KRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = true;
+#pragma message("enableValidationLayers=true")
+#else
+const bool enableValidationLayers = false;
+#pragma message("enableValidationLayers=false")
+#endif
+
+const char *test[] = {"val1", "val2"};
+
+std::string strVecFormat(const std::vector<const char *> &vec) {
+    std::string formatted = "{";
+    int idx = -1;
+    for (auto el: vec) {
+        idx += 1;
+        formatted += std::string(el);
+        if (!idx + 1 == vec.size()) {
+            formatted += ", ";
+        }
+    }
+    formatted += "}";
+    return formatted;
+}
+
+
+bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char *layerName: validationLayers) {
+        bool layerFound = false;
+
+        for (const auto &layerProperties: availableLayers) {
+            if (strcmp(layerProperties.layerName, layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 class TriangleApp {
 public:
@@ -39,11 +93,18 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
 
     void createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requested but not available");
+        } else {
+            std::cout << "Validation layers  found: " << strVecFormat(validationLayers);
+        }
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "basic";
@@ -64,7 +125,6 @@ private:
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         createInfo.enabledLayerCount = 0;
-
 
         if (vkCreateInstance(&createInfo, nullptr, &instance)) {
             throw std::runtime_error("failed to create instance!");
